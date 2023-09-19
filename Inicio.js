03ce05fba20df1591bd311c91a04a413f9a95e7e
SeeTzi <juniior.m2@hotmail.com>
2023-09-19 12:19:42 -0300

    Teste Alura primeiras aulas JS

---
+++ Inicio.js
@@ -0,0 +1,9 @@
+// Teste
+
+
+const nome = 1;
+const sobrenome = 2;
+
+const resultado = nome + sobrenome;
+
+console.log(resultado)
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo"
+console.log(citacao)
+
+
+//Concatenalção (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
