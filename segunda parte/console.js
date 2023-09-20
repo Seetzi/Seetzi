a88d8e85b6664e945ea9ee0e97ac228670acef82
SeeTzi <juniior.m2@hotmail.com>
2023-09-20 16:30:36 -0300

    Mais coisas aprendidas

---
+++ segunda parte/console.js
@@ -0,0 +1,12 @@
+// //Log ->> registro
+// const minhaVar = true;
+// console.log(245)
+// console.log("Eu sou um texto")
+// console.log(minhaVar)
+
+// //tratamento de error!
+
+// console.error('ops, algo deu errado')
+
+console.log("deu erro");
+console.error("deu erro");
---
+++ segunda parte/script.js
@@ -0,0 +1,5 @@
+// tipagem dinâmica
+
+let minhaVar = 567;
+minhaVar = "texto"
+minhaVar = true;
